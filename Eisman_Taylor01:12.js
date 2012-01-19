a9c629e39cfb66fafc094bede5aceb6b2af36139
TaylorEisman12 <taylor@theeismans.com>
2012-01-18 23:31:50 -0600

    Started Json on GitHub.com

--- Eisman_Taylor01:12.js
+++ Eisman_Taylor01:12.js
@@ -1,1 +1,26 @@
 alert("JavaScript works!");
+
+var json = {
+    "pets": {
+      "bird": {
+        "name": "Charley",
+        "color": "Orange",
+        "age": 4
+      },
+      "dog": {
+        "name": "Bevo",
+        "color": "Black",
+        "age": 2
+      },
+      "snake": {
+        "name": "Willy",
+        "color": "Green",
+        "age": 6
+      },
+      "cat": {
+        "name": "Sin",
+        "color": "Grey",
+        "age": 1
+      }
+    } 
+};
