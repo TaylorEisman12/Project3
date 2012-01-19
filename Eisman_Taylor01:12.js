3191fbded2db2c185c3b0d79570074998b38a362
TaylorEisman12 <taylor@theeismans.com>
2012-01-19 00:07:07 -0600

    Restructuring to see if it will help with story.

--- Eisman_Taylor01:12.js
+++ Eisman_Taylor01:12.js
@@ -24,3 +24,37 @@ var json = {
       }
     } 
 };
+
+for (var key in json.pets) {
+    var pet = json.pets[key];
+    pet.age
+};
+
+var json2 = {
+    "pets": [
+        {
+            "animal": "Bird",
+            "name": "Charley",
+            "color": "Orange",
+            "age": 4
+        },
+           {
+            "animal": "Dog",
+            "name": "Bevo",
+            "color": "Black",
+            "age": 2
+        },
+           {
+            "animal": "Snake",
+            "name": "Willey",
+            "color": "Green",
+            "age": 6
+        },
+           {
+            "animal": "Cat",
+            "name": "Sin",
+            "color": "Grey",
+            "age": 1
+        }
+    ]
+};
